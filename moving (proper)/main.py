91d86038213fd898cb8287e982eda23fd5a6805d
Ajal Varuthunny <ajalvaruthunny@gmail.com>
2020-05-01 10:14:49 +0200

    moving module completed moving regardles of the computer performance created. images added

--- moving (proper)/main.py
+++ moving (proper)/main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Rectangle
 from kivy.core.window import Window
+from kivy.clock import Clock
 
 
 class GameWidget(Widget):
@@ -10,27 +11,44 @@ class GameWidget(Widget):
         self._keyboard = Window.request_keyboard(
             self._on_keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_key_down)
+        self._keyboard.bind(on_key_up=self._on_key_up)
 
         with self.canvas:
             self.player = Rectangle(source="spaceship.png",
                                     pos=(0, 0), size=(100, 100))
 
+        self.keysPressed = set()
+
+        Clock.schedule_interval(self.move_step, 0)  # 0 means every frame
+
     def _on_keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_key_down)
+        self._keyboard.unbind(on_key_up=self._on_key_up)
         self._keyboard = None
 
     def _on_key_down(self, keyboard, keycode, text, modifiers):
+
+        self.keysPressed.add(text)
+
+    def _on_key_up(self, keyboard, keycode):
+        text = keycode[1]
+        if text in self.keysPressed:
+            self.keysPressed.remove(text)
+
+    def move_step(self, dt):  # called every frame
         currentx = self.player.pos[0]
         currenty = self.player.pos[1]
 
-        if text == "w":
-            currenty += 1
-        if text == "s":
-            currenty -= 1
-        if text == "a":
-            currentx -= 1
-        if text == "d":
-            currentx += 1
+        step_size = 100 * dt
+
+        if "w" in self.keysPressed:
+            currenty += step_size
+        if "s" in self.keysPressed:
+            currenty -= step_size
+        if "a" in self.keysPressed:
+            currentx -= step_size
+        if "d" in self.keysPressed:
+            currentx += step_size
 
         self.player.pos = (currentx, currenty)
 
